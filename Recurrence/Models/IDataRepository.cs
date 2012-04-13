c71399b0f34a6e009ccff6d1e5760d0675d71c48
Seth Webster <sethwebster@gmail.com>
2012-04-13 09:19:19 -0700

    Updated Data Model, added controller

--- Recurrence/Models/IDataRepository.cs
+++ Recurrence/Models/IDataRepository.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 
 namespace Recurrence.Models
 {
-    public interface IDataRepository
+    public interface IDataRepository : IDisposable
     {
         DbSet<Job> Jobs { get; set; }
         int SaveChanges();
+    }
+
+    public static class IDataRepositoryExtensions
+    {
+        public static DbEntityEntry Entry(this IDataRepository input, object entity)
+        {
+            return (input as DbContext).Entry(entity);
+        }
     }
 }
