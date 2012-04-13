ea412e1c4681e4e2bc339a2a8905e5b302718753
Seth Webster <sethwebster@gmail.com>
2012-04-13 09:03:24 -0700

    project created

---
+++ Recurrence/Areas/apiv1/apiv1AreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Recurrence.Areas.apiv1
+{
+    public class apiv1AreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "apiv1";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "apiv1_default",
+                "apiv1/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Recurrence/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Recurrence.MvcApplication" Language="C#" %>
